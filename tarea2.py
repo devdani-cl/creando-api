ac7dede189a6251f3124c38f76e42fb40d933537
Dani <di.avendanopinto@gmail.com>
2025-06-06 22:35:30 -0400

    practicando con endpoint

--- tarea2.py
+++ tarea2.py
@@ -1,4 +1,3 @@
-from Pokemon import Pokemon
 from random import  randint
 from fastapi import FastAPI
 import json
@@ -11,37 +10,20 @@ app = FastAPI()
 # Conexión a Redis (host viene de docker-compose)
 r = redis.Redis(host=os.getenv("REDIS_HOST", "localhost"), port=6379, decode_responses=True)
 
-@app.get("/cache/{key}") # endpoit
-def set_cache(key: str):
-    value = r.get(key)
-    if value:
-        return {"key": key, "value": value, "cached": True}
 
-    r.set(key, f'nuevo valor{key}', ex=3600)  # expira en 1 hora 
-    return {"key": key, "value": key, "cached": False}
+@app.get("/ping/")
+def ping():
+    return {"pong": True}
 
-@app.get("/usm/alumnos")
-def get_alumnos():
-    a = randint(0,100)
-    b = randint(0,100)
-    return [
-        {
-            "name": "Maximiliano",
-            "edad": a
-        },
-        {
-            "name": "Shaquira",
-            "edad": b
-        }
-    ]
+usuarios = ["asd", "sld", "lñd", "sñlf", "ada", "ñaa", "ida"]
 
-@app.get("usm/pokemones")
-def get_pokemon():
-    url = "https://pokeapi.co/api/v2/pokemon/?limit=151"
-    response = requests.get(url) 
-    poke_json = json.loads(response.text)
-    lista_pokemones = [Pokemon(n["name"], n["url"]) for n in poke_json["results"]]
-    for p in lista_pokemones:
-        hp = p.vidaPokemon()
-        print(f'{p.nombre}: {hp} HP')
-    
+@app.get("/usuarios/{id}")
+def get_idUsuario(id: int):
+    if 0 <= id < len(usuarios):
+        return {"id": id, "nombre": usuarios[id]}
+
+@app.get("/usuarios")
+def listar_usuarios():
+    return usuarios
+
+
